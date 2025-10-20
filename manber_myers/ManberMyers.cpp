eac0654c9372a7d04f0922ae98e2f6784a84a116
Santiago Rodriguez <rodriguezgutierrez.santiago05@gmail.com>
2025-10-19 23:59:11 -0600

    Done with ManberMyers suffixArray function

--- manber_myers/ManberMyers.cpp
+++ manber_myers/ManberMyers.cpp
@@ -36,6 +36,52 @@ vector<int> makeRanks(const vector<SubstrRank> &substr_rank, int n)
     return rank;
 }
 
+vector<int> suffixArray(const string &T)
+{
+    int n = T.size();
+    vector<SubstrRank> substr_rank;
+
+    for (int i = 0; i < n; i++)
+    {
+        int left = (int)T[i];
+        int right = (i < n - 1) ? (int)T[i + 1] : 0;
+        substr_rank.push_back(SubstrRank(left, right, i));
+    }
+
+    sort(substr_rank.begin(), substr_rank.end(), [](const SubstrRank &a, const SubstrRank &b)
+         {
+        if (a.leftRank == b.leftRank)
+            return a.rightRank < b.rightRank;
+        return a.leftRank < b.leftRank; });
+
+    int l = 2;
+    while (l < n)
+    {
+        vector<int> rank = makeRanks(substr_rank, n);
+
+        for (int i = 0; i < n; i++)
+        {
+            int nextIndex = substr_rank[i].index + l;
+            int right = (nextIndex < n) ? rank[nextIndex] : 0;
+            substr_rank[i] = SubstrRank(rank[substr_rank[i].index], right, substr_rank[i].index);
+        }
+
+        sort(substr_rank.begin(), substr_rank.end(), [](const SubstrRank &a, const SubstrRank &b)
+             {
+            if (a.leftRank == b.leftRank)
+                return a.rightRank < b.rightRank;
+            return a.leftRank < b.leftRank; });
+
+        l *= 2;
+    }
+
+    vector<int> SA(n);
+    for (int i = 0; i < n; i++)
+        SA[i] = substr_rank[i].index;
+
+    return SA;
+}
+
 int main()
 {
     string T = "mississippi";
